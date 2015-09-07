2343fd1754eb5a937f6c201d54d3a64a968d2280
AlphaTech <alphat3ch@icloud.com>
2015-09-07 04:52:19 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-{
+exports.settings = {
 	// Nickname of your bot
 	globalNickname: "ShapeShift",
 	// Username of your bot
@@ -7,12 +7,21 @@
 	globalRealname: "ShapeShift Crypto-Currency Converter",
 	// Command prefix for your bot
 	globalCommandPrefix: "!",
-  // NickServ Nickname followed by a space-colon followed by the identification message
+  	// NickServ Nickname followed by a space-colon followed by the identification message
 	globalNickServIdent: "NickServ :IDENTIFY ShapeShift Password",
+	// Use SASL Authentication?
+	globalSasl: false,
 	// Hostname or IP of the IRC Server
 	globalNetHostname: "chat.freenode.net",
-	// Port of The IRC Server
+	// Port of The IRC Server (Highly reccomended to use 6697 (SSL) on networks that support it)
 	globalNetPort: "6697",
 	// Use SSL? (Highly reccomended) (set as either true or false with no quotes)
-	globalSecure: ture
+	globalNetSecure: true,
+	// An array of channels to join
+	globalChannelList: ['#ShapeShift', '##ShapeShift', "###ShapeShift"],
+	// Bind address if you have multiple IP addresses (Leave null if you are not sure, or only have one IP)
+	globalNetAddr: null,
+	// Try to auto rejoin channels?
+	globalAutoRejoin: true,
+	
 }
