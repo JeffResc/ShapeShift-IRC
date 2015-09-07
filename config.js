e30bde6e1e8d09aa3f9863fb7d0d68b1093cde6c
AlphaTech <alphat3ch@icloud.com>
2015-09-07 04:44:10 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+{
+	// Nickname of your bot
+	globalNickname: "ShapeShift",
+	// Username of your bot
+	globalUsername: "ShapeShift",
+	// Realname of your bot
+	globalRealname: "ShapeShift Crypto-Currency Converter",
+	// Command prefix for your bot
+	globalCommandPrefix: "!",
+  // NickServ Nickname followed by a space-colon followed by the identification message
+	globalNickServIdent: "NickServ :IDENTIFY ShapeShift Password",
+	// Hostname or IP of the IRC Server
+	globalNetHostname: "chat.freenode.net",
+	// Port of The IRC Server
+	globalNetPort: "6697",
+	// Use SSL? (Highly reccomended) (set as either true or false with no quotes)
+	globalSecure: ture
+}
